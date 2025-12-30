08fe77b182b3de6f4e929079d3d0f76d1c1d6ac6
AdamIlnicki24 <ilnicki.adam24@gmail.com>
2025-12-30 21:01:10 +0100

    Change url

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -3,13 +3,13 @@
 import { Button } from "@/components/buttons/Button/Button";
 import { BACK_TO_HOMEPAGE_BUTTON_LABEL } from "@/constants/buttons";
 import { PAGE_DOES_NOT_EXIST } from "@/constants/errorMessages";
-import { HOME_URL } from "@/constants/urls";
+import { LOG_IN_URL } from "@/constants/urls";
 import { useRouter } from "next/navigation";
 
 export default function NotFound() {
   const router = useRouter();
 
-  const backToHomepageHandler = () => router.push(HOME_URL);
+  const backToHomepageHandler = () => router.push(LOG_IN_URL);
 
   return (
     <div className="mt-5 flex min-h-svh flex-col items-center justify-center gap-y-4">
