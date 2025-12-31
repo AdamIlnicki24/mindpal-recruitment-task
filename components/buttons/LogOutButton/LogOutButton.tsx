de3d5d4aaf41974ae7ef1dbe3de103a54be51b92
AdamIlnicki24 <ilnicki.adam24@gmail.com>
2025-12-31 19:07:57 +0100

    Create LogOutButton.tsx

---
+++ components/buttons/LogOutButton/LogOutButton.tsx
@@ -0,0 +1,40 @@
+"use client";
+
+import { Button } from "@/components/buttons/Button/Button";
+import {
+  LOGGING_OUT_BUTTON_LABEL,
+  LOG_OUT_BUTTON_LABEL,
+} from "@/constants/buttons";
+import { LOG_OUT_SUCCESS_TOAST } from "@/constants/toasts";
+import { LOG_IN_URL } from "@/constants/urls";
+import { supabase } from "@/supabase/supabaseClient";
+import { addToast } from "@heroui/react";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+
+export function LogOutButton() {
+  const [loading, setLoading] = useState(false);
+  const router = useRouter();
+
+  const handleLogOut = async () => {
+    setLoading(true);
+    const { error } = await supabase.auth.signOut();
+    if (error) {
+      addToast({ color: "danger", title: "Błąd wylogowania" });
+      setLoading(false);
+      return;
+    }
+    addToast({ color: "success", title: LOG_OUT_SUCCESS_TOAST });
+    router.replace(LOG_IN_URL);
+  };
+
+  return (
+    <Button
+      title={loading ? LOGGING_OUT_BUTTON_LABEL : LOG_OUT_BUTTON_LABEL}
+      onClick={handleLogOut}
+      disabled={loading}
+      size="sm"
+      className="bg-defaultGray"
+    />
+  );
+}
